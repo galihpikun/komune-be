9d547269c0fc5f316f868f824a3534ffba7dd8fd
galihpikun <galihrakha003@gmail.com>
2026-05-09 09:29:06 +0700

    fix:Change get forum by id to slug

--- src/controllers/forumController.js
+++ src/controllers/forumController.js
@@ -21,54 +21,71 @@ export const getForums = async (req, res) => {
   }
 };
 
-export const getForumById = async (req, res) => {
-    const forumId = req.params;
-    try {
-        const [forum] = await connection.query("SELECT * FROM forums WHERE id = ?", [forumId]);
+export const getForumBySlug = async (req, res) => {
+  try {
+    const { slug } = req.params;
 
-        if (forum.length === 0) {
-            return res.status(404).json({
-                code: 404,
-                message: "Forum dengan ID " + forumId + " tidak ditemukan",
-            });
-        }
+    const [forums] = await connection.query(
+      `
+            SELECT
+                forums.*,
 
-        return res.status(200).json({
-            code:200,
-            message: "Berhasil mengambil forum dengan ID " + forumId,
-            success: true,
-            data: forum
-        })
-    } catch (error) {
-        console.error(error);
-    return res.status(500).json({
-      code: 500,
-      message: "Gagal mengambil forum dengan ID" + forumId,
-    });
+                users.id AS creator_id,
+                users.username AS creator_username,
+                users.avatar AS creator_avatar,
+                COUNT(DISTINCT forum_members.id) AS total_members,
+COUNT(DISTINCT posts.id) AS total_posts
+
+            FROM forums
+
+            JOIN users
+            ON users.id = forums.created_by
+
+            WHERE forums.slug = ?
+            `,
+      [slug],
+    );
+
+    if (forums.length === 0) {
+      return res.status(404).json({
+        success: false,
+        message: "Forum tidak ditemukan",
+      });
     }
+
+    return res.status(200).json({
+      success: true,
+      data: forums[0],
+    });
+  } catch (error) {
+    return res.status(500).json({
+      success: false,
+      message: error.message,
+    });
+  }
 };
 
 export const createForum = async (req, res) => {
-    try {
-        const { name, description } = req.body;
+  try {
+    const { name, description } = req.body;
 
-        const userId = req.user.id;
+    const userId = req.user.id;
 
-        const slug = slugify(name, {
-            lower: true,
-            strict: true
-        });
+    const slug = slugify(name, {
+      lower: true,
+      strict: true,
+    });
 
-        const iconImage = req.files["icon_image"]
-            ? req.files["icon_image"][0].filename
-            : null;
+    const iconImage = req.files["icon_image"]
+      ? req.files["icon_image"][0].filename
+      : null;
 
-        const bannerImage = req.files["banner_image"]
-            ? req.files["banner_image"][0].filename
-            : null;
+    const bannerImage = req.files["banner_image"]
+      ? req.files["banner_image"][0].filename
+      : null;
 
-        const [result] = await connection.query(
-            `
+    const [result] = await connection.query(
+      `
             INSERT INTO forums
             (
                 name,
@@ -80,102 +97,86 @@ export const createForum = async (req, res) => {
             )
             VALUES (?, ?, ?, ?, ?, ?)
             `,
-            [
-                name,
-                slug,
-                description,
-                iconImage,
-                bannerImage,
-                userId
-            ]
-        );
+      [name, slug, description, iconImage, bannerImage, userId],
+    );
 
-        return res.status(201).json({
-            success: true,
-            message: "Forum berhasil dibuat",
-            data: {
-                id: result.insertId
-            }
-        });
-
-    } catch (error) {
-        return res.status(500).json({
-            success: false,
-            message: error.message
-        });
-    }
+    return res.status(201).json({
+      success: true,
+      message: "Forum berhasil dibuat",
+      data: {
+        id: result.insertId,
+      },
+    });
+  } catch (error) {
+    return res.status(500).json({
+      success: false,
+      message: error.message,
+    });
+  }
 };
 
 export const updateForum = async (req, res) => {
-    try {
-        const { id } = req.params;
-        const { name, description } = req.body;
+  try {
+    const { id } = req.params;
+    const { name, description } = req.body;
 
-        const user = req.user;
+    const user = req.user;
 
-        const [forums] = await connection.query(
-            "SELECT * FROM forums WHERE id = ?",
-            [id]
-        );
+    const [forums] = await connection.query(
+      "SELECT * FROM forums WHERE id = ?",
+      [id],
+    );
 
-        if (forums.length === 0) {
-            return res.status(404).json({
-                success: false,
-                message: "Forum tidak ditemukan"
-            });
-        }
+    if (forums.length === 0) {
+      return res.status(404).json({
+        success: false,
+        message: "Forum tidak ditemukan",
+      });
+    }
 
-        const forum = forums[0];
+    const forum = forums[0];
 
-        // authorization
-        const isOwner = forum.created_by === user.id;
-        const isAdmin =
-            user.role === "admin" ||
-            user.role === "super_admin";
+    // authorization
+    const isOwner = forum.created_by === user.id;
+    const isAdmin = user.role === "admin" || user.role === "super_admin";
 
-        if (!isOwner && !isAdmin) {
-            return res.status(403).json({
-                success: false,
-                message: "Tidak memiliki akses"
-            });
-        }
+    if (!isOwner && !isAdmin) {
+      return res.status(403).json({
+        success: false,
+        message: "Tidak memiliki akses",
+      });
+    }
 
-        const slug = name
-            ? slugify(name, {
-                  lower: true,
-                  strict: true
-              })
-            : forum.slug;
+    const slug = name
+      ? slugify(name, {
+          lower: true,
+          strict: true,
+        })
+      : forum.slug;
 
-        let iconImage = forum.icon_image;
-        let bannerImage = forum.banner_image;
+    let iconImage = forum.icon_image;
+    let bannerImage = forum.banner_image;
 
-        // replace icon
-        if (req.files["icon_image"]) {
-            if (forum.icon_image) {
-                fs.unlinkSync(
-                    `uploads/forums/${forum.icon_image}`
-                );
-            }
+    // replace icon
+    if (req.files["icon_image"]) {
+      if (forum.icon_image) {
+        fs.unlinkSync(`uploads/forums/${forum.icon_image}`);
+      }
 
-            iconImage =
-                req.files["icon_image"][0].filename;
-        }
+      iconImage = req.files["icon_image"][0].filename;
+    }
 
-        // replace banner
-        if (req.files["banner_image"]) {
-            if (forum.banner_image) {
-                fs.unlinkSync(
-                    `uploads/forums/${forum.banner_image}`
-                );
-            }
+    // replace banner
+    if (req.files["banner_image"]) {
+      if (forum.banner_image) {
+        fs.unlinkSync(`uploads/forums/${forum.banner_image}`);
+      }
 
-            bannerImage =
-                req.files["banner_image"][0].filename;
-        }
+      bannerImage = req.files["banner_image"][0].filename;
+    }
 
-        await connection.query(
-            `
+    await connection.query(
+      `
             UPDATE forums
             SET
                 name = ?,
@@ -185,90 +186,79 @@ export const updateForum = async (req, res) => {
                 banner_image = ?
             WHERE id = ?
             `,
-            [
-                name || forum.name,
-                slug,
-                description || forum.description,
-                iconImage,
-                bannerImage,
-                id
-            ]
-        );
+      [
+        name || forum.name,
+        slug,
+        description || forum.description,
+        iconImage,
+        bannerImage,
+        id,
+      ],
+    );
 
-        return res.status(200).json({
-            success: true,
-            message: "Forum berhasil diupdate"
-        });
-
-    } catch (error) {
-        return res.status(500).json({
-            success: false,
-            message: error.message
-        });
-    }
+    return res.status(200).json({
+      success: true,
+      message: "Forum berhasil diupdate",
+    });
+  } catch (error) {
+    return res.status(500).json({
+      success: false,
+      message: error.message,
+    });
+  }
 };
 
 export const deleteForum = async (req, res) => {
-    try {
-        const { id } = req.params;
+  try {
+    const { id } = req.params;
 
-        const user = req.user;
+    const user = req.user;
 
-        const [forums] = await connection.query(
-            "SELECT * FROM forums WHERE id = ?",
-            [id]
-        );
+    const [forums] = await connection.query(
+      "SELECT * FROM forums WHERE id = ?",
+      [id],
+    );
 
-        if (forums.length === 0) {
-            return res.status(404).json({
-                success: false,
-                message: "Forum tidak ditemukan"
-            });
-        }
-
-        const forum = forums[0];
-
-        // authorization
-        const isOwner = forum.created_by === user.id;
-        const isAdmin =
-            user.role === "admin" ||
-            user.role === "super_admin";
-
-        if (!isOwner && !isAdmin) {
-            return res.status(403).json({
-                success: false,
-                message: "Tidak memiliki akses"
-            });
-        }
-
-        // delete icon
-        if (forum.icon_image) {
-            fs.unlinkSync(
-                `uploads/forums/${forum.icon_image}`
-            );
-        }
-
-        // delete banner
-        if (forum.banner_image) {
-            fs.unlinkSync(
-                `uploads/forums/${forum.banner_image}`
-            );
-        }
-
-        await connection.query(
-            "DELETE FROM forums WHERE id = ?",
-            [id]
-        );
-
-        return res.status(200).json({
-            success: true,
-            message: "Forum berhasil dihapus"
-        });
-
-    } catch (error) {
-        return res.status(500).json({
-            success: false,
-            message: error.message
-        });
+    if (forums.length === 0) {
+      return res.status(404).json({
+        success: false,
+        message: "Forum tidak ditemukan",
+      });
     }
+
+    const forum = forums[0];
+
+    // authorization
+    const isOwner = forum.created_by === user.id;
+    const isAdmin = user.role === "admin" || user.role === "super_admin";
+
+    if (!isOwner && !isAdmin) {
+      return res.status(403).json({
+        success: false,
+        message: "Tidak memiliki akses",
+      });
+    }
+
+    // delete icon
+    if (forum.icon_image) {
+      fs.unlinkSync(`uploads/forums/${forum.icon_image}`);
+    }
+
+    // delete banner
+    if (forum.banner_image) {
+      fs.unlinkSync(`uploads/forums/${forum.banner_image}`);
+    }
+
+    await connection.query("DELETE FROM forums WHERE id = ?", [id]);
+
+    return res.status(200).json({
+      success: true,
+      message: "Forum berhasil dihapus",
+    });
+  } catch (error) {
+    return res.status(500).json({
+      success: false,
+      message: error.message,
+    });
+  }
 };
